0860446b3cff9d8670676e3f7e652df2bcaae3d3
ermvrs <ermvrs@live.com>
2025-09-14 16:58:41 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use miden_client_tools::{
 
 use miden_client::{
     account::{StorageSlot},
-    Word, keystore::FilesystemKeyStore, rpc::Endpoint, transaction::TransactionRequestBuilder, Felt
+    Word, rpc::Endpoint, transaction::TransactionRequestBuilder, Felt
 };
 use miden_objects::account::NetworkId;
 use tokio::time::{Duration, sleep};
