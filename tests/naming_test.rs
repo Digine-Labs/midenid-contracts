50b16693ba275ed69482f41b11f231e338d4a8a1
ermvrs <ermvrs@live.com>
2025-10-11 23:50:26 +0300

    failing test

--- tests/naming_test.rs
+++ tests/naming_test.rs
@@ -1,26 +1,49 @@
-use miden_client::account::AccountStorage;
-use miden_lib::transaction::memory::StorageSlot;
-
-use crate::utils::{empty_storage_map, empty_storage_value};
+use miden_client::{note::{NoteExecutionMode, NoteTag}, testing::NoteBuilder};
+use miden_crypto::Felt;
+use miden_testing::{MockChainBuilder};
+use rand_chacha::ChaCha20Rng;
+use rand::{Rng, SeedableRng};
+use miden_objects::note::{NoteType};
+use crate::utils::{create_account, create_naming_account, create_naming_library, get_note_code};
 mod utils;
 
-fn naming_storage() -> Vec<StorageSlot> {
-    let storage_slots: Vec<StorageSlot> = vec![
-        empty_storage_value(), // Init flag
-        empty_storage_value(), // owner
-        empty_storage_value(), // treasury
-        empty_storage_map(), // payment token -> price contract
-        empty_storage_map(), // account to domain
-        empty_storage_map(), // domain to account
-        empty_storage_map(), // domain to owner
-        ];
-    return storage_slots;
-}
+
 
 // Develop test like that
 // https://github.com/0xMiden/miden-base/blob/719ff03d1482e6ce2ad4e986f59ec7b9a8ddf962/crates/miden-testing/src/kernel_tests/tx/test_fpi.rs#L515
 
 #[tokio::test]
-async fn test_naming_initialize() -> Result<()>{
-    let storage_slots = vec![AccountStorage::mock_item_0().slot]
+async fn test_naming_initialize() -> anyhow::Result<()>{
+    let owner_account = create_account()?;
+    let treasury_account = create_account()?;
+    let naming_account = create_naming_account();
+
+    let mut mock_chain = MockChainBuilder::with_accounts([naming_account.clone()]).unwrap().build()?;
+    mock_chain.prove_next_block()?;
+
+    // Reverse ordered. TODO: create utility function to reverse order words.
+    let initialize_inputs = vec![
+        Felt::new(treasury_account.id().suffix().into()), 
+        Felt::new(treasury_account.id().prefix().into()),
+        Felt::new(owner_account.id().suffix().into()),
+        Felt::new(owner_account.id().prefix().into())
+    ];
+
+    let naming_library = create_naming_library().unwrap();
+    
+    let note= NoteBuilder::new(owner_account.id(), ChaCha20Rng::from_os_rng())
+        .code(get_note_code("initialize".to_string()))
+        .note_type(NoteType::Public)
+        .tag(NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Network).unwrap().into())
+        .add_assets(vec![])
+        .dynamically_linked_libraries(vec![naming_library])
+        .note_inputs(initialize_inputs.clone()).unwrap().build()?;
+
+    let tx_context = mock_chain.build_tx_context(owner_account.id(), &[note.id()], &[note])
+        .expect("failed to build tx")
+        .build()?;
+
+    tx_context.execute();
+    
+    Ok(())
 }
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,5 +1,30 @@
-use miden_client::account::{StorageMap, StorageSlot};
+use std::{fs, path::Path, sync::Arc};
+
+use miden_assembly::{ast::{Module, ModuleKind}, Assembler, DefaultSourceManager, Library, LibraryPath};
+use miden_client::{account::{AccountBuilder, AccountType, StorageMap, StorageSlot}, crypto::SecretKey, testing::NoteBuilder};
 use miden_crypto::{Felt, Word};
+use miden_objects::account::{AccountComponent, AccountStorageMode, Account, AccountId};
+use miden_objects::note::{NoteType};
+use miden_lib::{account::{auth, wallets::BasicWallet}, transaction::TransactionKernel};
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha20Rng;
+
+pub fn naming_storage() -> Vec<StorageSlot> {
+    let storage_slots: Vec<StorageSlot> = vec![
+        empty_storage_value(), // Init flag
+        empty_storage_value(), // owner
+        empty_storage_value(), // treasury
+        empty_storage_map(), // payment token -> price contract
+        empty_storage_map(), // account to domain
+        empty_storage_map(), // domain to account
+        empty_storage_map(), // domain to owner
+        ];
+    return storage_slots;
+}
+
+mod paths {
+    pub const NAMING_ACCOUNT: &str = "./masm/accounts/naming.masm";
+}
 
 pub fn empty_storage_value() -> StorageSlot {
     StorageSlot::Value(Word::new([
@@ -12,4 +37,59 @@ pub fn empty_storage_value() -> StorageSlot {
 
 pub fn empty_storage_map() -> StorageSlot {
     StorageSlot::Map(StorageMap::new())
+}
+
+pub fn get_naming_account_code() -> String {
+    fs::read_to_string(Path::new(paths::NAMING_ACCOUNT)).unwrap()
+}
+
+pub fn get_note_code(note_name: String) -> String {
+    fs::read_to_string(Path::new(&format!("./masm/notes/{}.masm", note_name))).unwrap()
+}
+
+pub fn create_account() -> anyhow::Result<Account> {
+    let mut rng = ChaCha20Rng::from_os_rng();
+    let key_pair = SecretKey::with_rng(&mut rng);
+    let (account, seed) = AccountBuilder::new(ChaCha20Rng::from_os_rng().random())
+        .account_type(AccountType::RegularAccountUpdatableCode)
+        .storage_mode(AccountStorageMode::Public)
+        .with_auth_component(auth::AuthRpoFalcon512::new(key_pair.public_key().clone()))
+        .with_component(BasicWallet).build()?;
+
+    Ok(account)
+}
+
+pub fn create_naming_account() -> Account {
+    let storage_slots = naming_storage();
+    let account_code = get_naming_account_code();
+
+    let account_component = AccountComponent::compile(
+        account_code.clone(), 
+        TransactionKernel::assembler().with_debug_mode(true), 
+        storage_slots
+    ).unwrap().with_supports_all_types();
+
+    let account = AccountBuilder::new(ChaCha20Rng::from_os_rng().random())
+        .with_auth_component(auth::NoAuth)
+        .with_component(account_component)
+        .storage_mode(AccountStorageMode::Public)
+        .build_existing().unwrap();
+    return account;
+}
+
+pub fn create_public_note(note_name: String, sender: AccountId) {
+
+}
+
+pub fn create_naming_library() -> Result<Library, Box<dyn std::error::Error>> {
+    let account_code = get_naming_account_code();
+    let assembler: Assembler = TransactionKernel::assembler().with_debug_mode(true);
+    let source_manager = Arc::new(DefaultSourceManager::default());
+    let module = Module::parser(ModuleKind::Library).parse_str(
+        LibraryPath::new("miden_name::naming")?,
+        account_code,
+        &source_manager,
+    )?;
+    let library = assembler.clone().assemble_library([module])?;
+    Ok(library)
 }
