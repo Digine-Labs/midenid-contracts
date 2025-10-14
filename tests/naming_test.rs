76eea6286d525ab1264ee5347bc5cd1e661ce9ee
ermvrs <ermvrs@live.com>
2025-10-14 11:36:00 +0300

    test with mockchain

--- tests/naming_test.rs
+++ tests/naming_test.rs
@@ -23,11 +23,11 @@ async fn test_naming_init() -> anyhow::Result<()> {
 
     let naming_account = create_naming_account();
 
-    let initialize_input_note = create_naming_initialize_note(owner_account, treasury_account, naming_account.clone()).await.unwrap();
+    let initialize_input_note = create_naming_initialize_note(owner_account.clone(), treasury_account.clone(), naming_account.clone()).await.unwrap();
 
     builder.add_note(OutputNote::Full(initialize_input_note.clone()));
 
-    let mock_chain = builder.build()?;
+    let mut mock_chain = builder.build()?;
 
     let tx_inputs = mock_chain.get_transaction_inputs(naming_account.clone(), None, &[initialize_input_note.id()], &[])?;
 
@@ -37,54 +37,16 @@ async fn test_naming_init() -> anyhow::Result<()> {
         .build()?;
 
     let _exec_tx = tx_context.execute().await?;
-    Ok(())
-}
-
-#[tokio::test]
-#[ignore = "reason"]
-async fn test_naming_initialize() -> anyhow::Result<()>{
-    let owner_account = create_account()?;
-    let treasury_account = create_account()?;
-    let naming_account = create_naming_account();
-
-
-    // Reverse ordered. TODO: create utility function to reverse order words.
-    let initialize_inputs = vec![
-        Felt::new(treasury_account.id().suffix().into()), 
-        Felt::new(treasury_account.id().prefix().into()),
-        Felt::new(owner_account.id().suffix().into()),
-        Felt::new(owner_account.id().prefix().into())
-    ];
-
-    let naming_library = create_naming_library().unwrap();
+    let updated_naming_account = mock_chain.add_pending_executed_transaction(&_exec_tx)?;
     
-    let note= NoteBuilder::new(owner_account.id(), ChaCha20Rng::from_os_rng())
-        .code(get_note_code("initialize".to_string()))
-        .note_type(NoteType::Public)
-        .tag(NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Network).unwrap().into())
-        .add_assets(vec![])
-        .dynamically_linked_libraries(vec![naming_library])
-        .note_inputs(initialize_inputs.clone()).unwrap().build()?;
+    let init_flag = updated_naming_account.storage().get_item(0)?.get(0).unwrap().as_int();
+    let owner_slot = updated_naming_account.storage().get_item(1)?;
+    let trasury_slot = updated_naming_account.storage().get_item(2)?;
 
-    let mut mock_chain = MockChainBuilder::with_accounts([naming_account.clone(), owner_account.clone(), treasury_account.clone()]).unwrap()
-    .build()?;
-    mock_chain.prove_next_block()?;
-
-    let sender_account_interface = AccountInterface::from(&owner_account);
-
-    let send_note_tx_script = sender_account_interface.build_send_notes_script(
-        slice::from_ref(&note.clone().into()), 
-        Some(10u16), 
-        true
-    )?;
-
-    let _executed_tx = mock_chain
-        .build_tx_context(owner_account.id(), &[], &[])
-        .expect("failed to build tx context")
-        .tx_script(send_note_tx_script)
-        .build()?
-        .execute()
-        .await?;
-    
+    assert_eq!(init_flag, 1);
+    assert_eq!(owner_account.id().prefix().as_u64(), owner_slot.get(1).unwrap().as_int());
+    assert_eq!(owner_account.id().suffix().as_int(), owner_slot.get(0).unwrap().as_int());
+    assert_eq!(treasury_account.id().prefix().as_u64(), trasury_slot.get(1).unwrap().as_int());
+    assert_eq!(treasury_account.id().suffix().as_int(), trasury_slot.get(0).unwrap().as_int());
     Ok(())
 }
